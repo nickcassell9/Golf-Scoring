49621f6eb8a75936f01700375e7553e4be5aa184
Nick Cassell <nick@strivr.com>
2018-08-05 08:52:40 -0400

    simple front end

--- public/app.js
+++ public/app.js
@@ -1,10 +1,26 @@
 const CreateUser = document.querySelector('.CreateUser')
 CreateUser.addEventListener('submit', (e) => {
+    console.log('bitch')
     e.preventDefault();
-    const username = CreateUser.querySelector('.username').value;
+    const email = CreateUser.querySelector('.email').value;
     const password = CreateUser.querySelector('.password').value;
-    post('/createUser', { username, password })
+    const first_name = CreateUser.querySelector('.first_name').value;
+    const last_name = CreateUser.querySelector('.last_name').value;
+    post('/user/', { email, password, first_name, last_name })
 })
+
+const Login = document.querySelector('.Login')
+Login.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const email = Login.querySelector('.email').value;
+    const password = Login.querySelector('.password').value;
+    post('/user/login', { email, password })
+        .then(({ status }) => {
+            if (status === 200) alert('login success')
+            else alert('login failed')
+        })
+});
+
 function post (path, data) {
     return window.fetch(path, {
         method: 'POST',
